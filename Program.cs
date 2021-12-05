b434b9fa39796c6e6449bc4f505e85e2198629df
Recaedae <lemour@live.co.uk>
2021-12-05 15:27:21 +0200

    Added CountDigits, ReduceDigits, IsAlphabetic to the Utility class.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace TennisMatch
 {
@@ -45,6 +46,63 @@ namespace TennisMatch
 
             return CountsList;
         }
+
+        public int CountDigits(int number)
+        {
+            return number == 0 ? 1 : (int) Math.Floor(Math.Log10(Math.Abs(number)) + 1);
+        }
+
+        public int ReduceDigits(List<int> counts)
+        {
+            while (counts.Count > 2)
+            {
+                int i = 0;
+                int j = counts.Count - 1;
+                List<int> NewCounts = new List<int>();
+                while (i <= j)
+                {
+                    int Num;
+                    // Add the leftmost and righmost numbers
+                    if (i == j)
+                    {
+                        Num = counts[i];
+                    }
+                    else
+                    {
+                        Num = counts[i] + counts[j];
+                    }
+
+                    // Check if Num is a single- or 2-digit number
+                    if (CountDigits(Num) > 1)
+                    {
+                        NewCounts.Add((int) (Num / 10));
+                        NewCounts.Add(Num % 10);
+                    }
+                    else
+                    {
+                        NewCounts.Add(Num);
+                    }
+
+                    i += 1;
+                    j -=1;
+                }
+
+                counts = NewCounts;
+            }
+
+            if (counts.Count == 2)
+            {
+                return counts[0] * 10 + counts[1];
+            }
+
+            return counts[0];
+        }
+
+        public bool IsAlphabetic(string str)
+        {
+            return Regex.IsMatch(str, @"^[a-zA-Z]+$");
+
+        }
     }
 
     class Program
@@ -58,12 +116,23 @@ namespace TennisMatch
             string Name1 = "mona";
             string Name2 = "lisa";
 
-            // Test CreateMatchString
-            Console.WriteLine(Ut.CreateMatchString(Name1, Name2, MidStr));
+            // // Test CreateMatchString
+            // Console.WriteLine(Ut.CreateMatchString(Name1, Name2, MidStr));
 
-            // Test CountMatchChars
-            List<int> CountsList = Ut.CountChars(Name1, Name2, MidStr);
-            Console.WriteLine("CountsList: " + String.Join(", ", CountsList));
+            // // Test CountMatchChars
+            // List<int> CountsList = Ut.CountChars(Name1, Name2, MidStr);
+            // Console.WriteLine("CountsList: " + String.Join(", ", CountsList));
+
+            // Test CountDigits
+            Console.WriteLine(Ut.CountDigits(-100000000)); // expect 9
+
+            // Test ReduceDigits
+            List<int> Counts = new List<int> {2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2};
+            Console.WriteLine(Ut.ReduceDigits(Counts)); // expect: 60
+
+            // Test IsAlphabetic
+            string Str = "@abdc";
+            Console.WriteLine(Ut.IsAlphabetic(Str));
         }
     }
 }
