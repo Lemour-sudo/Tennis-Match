ec08d5070c2d7a816dea13ac8fe3c1fe29f13765
Recaedae <lemour@live.co.uk>
2021-12-05 15:14:19 +0200

    Setup the console app project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TennisMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's talk it out at the tennis court...");
+        }
+    }
+}
