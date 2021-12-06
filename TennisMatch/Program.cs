32d126b4fe92002614df0dcf7c0f9705e2cb8d42
Recaedae <lemour@live.co.uk>
2021-12-06 23:08:25 +0200

    Did user input functionality in console. And code now checks input in console.

--- TennisMatch/Program.cs
+++ TennisMatch/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using HelperLibrary;
 
 namespace TennisMatch
@@ -64,20 +65,86 @@ namespace TennisMatch
 
     class Program
     {
+        private static string IOFolder = "IOFolder/";
+        private static string InpFileName;
+        private static string OutFileName = "output.txt";
+
+        private static string FetchUserInput()
+        {
+            string UserFileName = "";
+
+            Console.WriteLine("Please input a valid csv input file-name.");
+            Console.WriteLine("Make sure the csv file is available in the 'IOFolder' folder.");
+            Console.WriteLine("The file-name should end with: .csv");
+            Console.WriteLine("\n-------------------------------------------------------------");
+            Console.WriteLine("\nEnter file name below and hit Enter to continue:");
+
+            bool ValidInput = false;
+            while (!ValidInput)
+            {
+                UserFileName = Console.ReadLine();
+
+                // Check if input is at least 5 characters long
+                if (UserFileName.Length < 5)
+                {
+                    Console.WriteLine($"\nYou entered: '{UserFileName}', but a valid file-name should be at least 5 characters long, ending with: .csv");
+                    Console.WriteLine("\n-------------------------------------------------------------");
+                    Console.WriteLine("\nPlease enter a valid file-name and press Enter to continue (Or press Ctr-C to quit):");
+                    continue;
+                }
+
+                // Check if input ends with .csv
+                if (UserFileName.Substring(UserFileName.Length - 4) != ".csv")
+                {
+                    Console.WriteLine($"\nYou entered: '{UserFileName}', but a file-name ending with '.csv' is expected.");
+                    Console.WriteLine("\n-------------------------------------------------------------");
+                    Console.WriteLine("\nPlease enter a valid file-name and press Enter to continue (Or press Ctr-C to quit):");
+                    continue;
+                }
+
+                // Check if file exists
+                if (File.Exists(IOFolder + UserFileName))
+                {
+                    ValidInput = true;
+                }
+                else if (File.Exists("../" + IOFolder + UserFileName))
+                {
+                    IOFolder = "../" + IOFolder;
+                    ValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine($"\n'{UserFileName}' does not exist in '{IOFolder}'");
+                    Console.WriteLine("\n-------------------------------------------------------------");
+                    Console.WriteLine($"\nPlease enter an existing csv file in '{IOFolder}' and press Enter to continue (Or press Ctr-C to quit):");
+                }
+
+            }
+
+            Console.WriteLine($"\n'{UserFileName}' found in '{IOFolder}'!");
+            Console.WriteLine("\nProceeding to the next step...");
+            Console.WriteLine("\n-------------------------------------------------------------\n");
+
+            InpFileName = UserFileName;
+
+            return UserFileName;
+        }
+
         static void Main(string[] args)
         {
-            const string MidStr = "matches";
-            const int MinGoodScore = 80;     // Minimum good match score
+            Console.WriteLine("\nWelcome to Players-MatchUp!");
+            Console.WriteLine("===========================\n");
+
+            // const string MidStr = "matches";
+            // const int MinGoodScore = 80;     // Minimum good match score
 
             // // Run local tests
             // Test TestC = new Test();
             // TestC.RunLocalTests();
 
-            const string IOFolder = "IOFolder/";
-            const string InpFileName = IOFolder + "names_1.csv";
-            const string OutFileName = IOFolder + "output_1.txt";
+            FetchUserInput();
 
-            CSVReader CReader = new CSVReader(InpFileName);
+            CSVReader CReader = new CSVReader(IOFolder + InpFileName);
 
             // Read Females and Males sets from csv file
             List<HashSet<string>> AllNames = CReader.ReadCSV();
@@ -110,7 +177,7 @@ namespace TennisMatch
             {
                 MatchResults.Add(MatchObj.GetFinalMatchString());
             }
-            Utility.SaveListToFile(OutFileName, MatchResults);
+            Utility.SaveListToFile(IOFolder+OutFileName, MatchResults);
         }
     }
 }
